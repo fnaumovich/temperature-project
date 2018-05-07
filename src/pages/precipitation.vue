dce5b2b0acdefa15e1d85b29f4c33e730b179b32
Fedor Naumovich <fnaumovich@htc-cs.ru>
2018-05-07 17:57:05 +0400

    add chart

--- src/pages/precipitation.vue
+++ src/pages/precipitation.vue
@@ -1,11 +1,17 @@
 <template>
     <section>
         <div class="date-pickers">
-            <datepicker class="date-picker date-picker__start" v-model="dateStart" :format="format"></datepicker>
-            <datepicker class="date-picker date-picker__end" v-model="dateEnd" :format="format"></datepicker>
+            <datepicker class="date-picker date-picker__start" v-model="dateStart" :format="format" @selected="onSelectDay"></datepicker>
+            <datepicker class="date-picker date-picker__end" v-model="dateEnd" :format="format" @selected="onSelectDay"></datepicker>
         </div>
         <div class="graphic-wrapper">
-
+            <LineChart
+                :key="renderKey"
+                :data="precipitationValue"
+                :options="{ responsive: false, maintainAspectRatio: false }"
+                :width="900"
+                :height="600"
+            ></LineChart>
         </div>
     </section>
 </template>
@@ -13,6 +19,7 @@
 <script>
 import { mapState, mapActions } from 'vuex';
 import Datepicker from 'vuejs-datepicker';
+import LineChart from '@/components/LineChart.js';
 
 export default {
     data() {
@@ -20,10 +27,12 @@ export default {
             dateStart: 0,
             dateEnd: 0,
             format: 'yyyy-MM-dd',
+            renderKey: 0,
         };
     },
     components: {
         Datepicker,
+        LineChart,
     },
     computed: {
         formattedDateStart() {
@@ -37,6 +46,23 @@ export default {
                 return item.t >= this.formattedDateStart && item.t <= this.formattedDateEnd;
             });
         },
+        precipitationValue() {
+            const precipitationValues = [];
+
+            this.precipitationRange.forEach(item => {
+                precipitationValues.push(item.v);
+            });
+
+            return {
+                datasets: [
+                    {
+                        label: 'Data One',
+                        backgroundColor: '#f87979',
+                        data: precipitationValues,
+                    }
+                ]
+            }
+        },
 
         ...mapState('precipitation', [ 'precipitation' ]),
     },
@@ -48,6 +74,9 @@ export default {
             const day = String(newDate.getDate()).padStart(2, '0');
 
             return `${ year }-${ month }-${ day }`;
+        },
+        onSelectDay() {
+            this.renderKey = Math.floor(Math.random() * 1000);
         },
 
         ...mapActions('precipitation', [ 'fetchPrecipitation' ]),
